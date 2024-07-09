563bb55edce9eaf7cce87af0217b4371a900b9db
fabiolajsm <suarezfabiola17@gmail.com>
2024-07-09 15:51:46 -0300

    Sprint 3: Agregados completos/descarga excel

--- src/app/components/manageUsers/users-table/users-table.component.ts
+++ src/app/components/manageUsers/users-table/users-table.component.ts
@@ -1,11 +1,14 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ReactiveFormsModule } from '@angular/forms';
 import { Router, RouterModule } from '@angular/router';
 import { NgxSpinnerModule, NgxSpinnerService } from 'ngx-spinner';
 import { UserInterface } from '../../../interfaces/user.interface';
 import { AuthService } from '../../../services/auth.service';
 import { SectionTitleDirective } from '../../../directives/section-title.directive';
+import { Appointment } from '../../../interfaces/appointment.interface';
+import { SweetAlert2Module } from '@sweetalert2/ngx-sweetalert2';
+import * as XLSX from 'xlsx';
 
 @Component({
   selector: 'app-users-table',
@@ -16,12 +19,14 @@ import { SectionTitleDirective } from '../../../directives/section-title.directi
     RouterModule,
     NgxSpinnerModule,
     SectionTitleDirective,
+    SweetAlert2Module,
   ],
   templateUrl: './users-table.component.html',
 })
 export class UsersTableComponent {
   users: UserInterface[] = [];
   @Output() public userSelected: EventEmitter<any> = new EventEmitter<any>();
+  @Input() appointments: Appointment[] | undefined;
 
   constructor(
     private router: Router,
@@ -50,7 +55,63 @@ export class UsersTableComponent {
       (item) => item.email === userEmail
     )[0];
     this.userSelected.emit(userSelected);
+
+    const userData = {
+      Usuario: `${userSelected?.email || 'N/A'}`,
+      Nombre: `${userSelected?.name || 'N/A'} ${
+        userSelected?.lastname || 'N/A'
+      }`,
+      Edad: userSelected?.age || 'N/A',
+      DNI: userSelected?.dni || 'N/A',
+    };
+
+    if (userSelected && this.appointments) {
+      const filterApp = this.appointments.filter((item) => {
+        return userSelected.role === 'PACIENTE'
+          ? item.patient_id === userSelected.id
+          : false;
+      });
+
+      const detail: any[] = [];
+      filterApp.forEach((app: Appointment) => {
+        const infoTurnoExcel = {
+          Fecha: app.date,
+          Horario: `${app.start_time} - ${app.end_time} hs`,
+          Estado: app.status,
+          Especialidad: app.specialty,
+          NombrePaciente: app.patient_name,
+          Especialista: app.professional_name,
+        };
+        detail.push(infoTurnoExcel);
+      });
+
+      const wsUserData = XLSX.utils.json_to_sheet([userData]);
+      const wb = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, wsUserData, 'Información Usuario');
+
+      if (filterApp.length > 0) {
+        const wsAppointments = XLSX.utils.json_to_sheet(detail);
+        XLSX.utils.book_append_sheet(wb, wsAppointments, 'Turnos');
+      }
+
+      XLSX.writeFile(
+        wb,
+        `${userSelected.name}-${userSelected.lastname}-Informacion.xlsx`
+      );
+    } else {
+      const wsUserData = XLSX.utils.json_to_sheet([userData]);
+      const wb = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(wb, wsUserData, 'Información Usuario');
+
+      XLSX.writeFile(
+        wb,
+        `${userSelected?.name || 'Usuario'}-${
+          userSelected?.lastname || 'Info'
+        }.xlsx`
+      );
+    }
   }
+
   handleBack() {
     this.router.navigate(['/']);
   }
